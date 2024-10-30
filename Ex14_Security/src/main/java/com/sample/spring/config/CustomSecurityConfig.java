9741a00c77e9c95fdb44ab54e3884da5b3978886
chaelin2 <gcl693427@gmail.com>
2024-10-30 15:40:49 +0900

    카카오 로그인

--- Ex14_Security/src/main/java/com/sample/spring/config/CustomSecurityConfig.java
+++ Ex14_Security/src/main/java/com/sample/spring/config/CustomSecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +18,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import com.sample.spring.security.APILoginFailHandler;
 import com.sample.spring.security.APILoginSuccessHandler;
 import com.sample.spring.security.filter.JWTCheckFilter;
+import com.sample.spring.security.handler.CustomAccessDeniedHandler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -24,6 +26,7 @@ import lombok.extern.log4j.Log4j2;
 @Configuration
 @Log4j2
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class CustomSecurityConfig {
 
 	@Bean
@@ -42,12 +45,15 @@ public class CustomSecurityConfig {
 				config -> {
 					config.loginPage("/api/member/login");
 					config.successHandler(new APILoginSuccessHandler());
-//					config.failureHandler(new APILoginFailHandler());
+					config.failureHandler(new APILoginFailHandler());
 				}
 				);
 		
 		http.addFilterBefore(new JWTCheckFilter(), UsernamePasswordAuthenticationFilter.class);
 		
+		http.exceptionHandling(config -> {
+			config.accessDeniedHandler(new CustomAccessDeniedHandler());
+		});
 		return http.build();
 	}
 	
@@ -59,7 +65,7 @@ public class CustomSecurityConfig {
 	@Bean
 	public CorsConfigurationSource CorsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("HEAD","GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization","Cache-Control","Content-Type"));
         configuration.setAllowCredentials(true);
--- Ex14_Security/src/main/java/com/sample/spring/controller/HomeController.java
+++ Ex14_Security/src/main/java/com/sample/spring/controller/HomeController.java
@@ -1,14 +1,27 @@
 package com.sample.spring.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-	
+	@PreAuthorize("hasAnyRole('ROLE_MEMBER')")
 	@GetMapping("/test")
 	public String testPage() {
 		return "test";
+	}
+	
+	@GetMapping("/api/test")
+	public Map<String, Object> testView(){
+		Map<String, Object> response = new HashMap<>();
+		response.put("memssage", "test message");
+		response.put("id", 1234567);
+		
+		return response;
 	}
 }
